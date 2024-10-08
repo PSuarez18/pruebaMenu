29c2a994904c42ed5d7ad455ff5d943876edddd8
Paolo Suarez <ptsuarez91@hotmail.com>
2024-10-08 14:47:36 -0400

    correccion a erores

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -52,9 +52,7 @@ const Profile = () => {
                     </select>
                 </div>
             </div>
-            <div className="px-5 pb-5 mx-auto">
-                <ButtonSecondary textChildren={LOGOUT_TEXT} onClick={handleLogoutClick} className="w-full  text-white" />
-            </div>
+           
         </div>
     );
 };
